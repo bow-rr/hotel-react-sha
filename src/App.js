077fa1967a67606ad5ae13a46fb9e1239457a767
Howard <howardsoon@gmail.com>
2023-03-12 00:02:28 +0000

    complete

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
+import Bookings from "./Bookings";
+import Restaurant from "./Restaurant";
+import Footer from "./Footer";
+import "./App.css";
+
+const App = () => {
+  const contactDetails = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789"
+  ];
+
+  return (
+    <div className="App">
+      <Heading />
+      <TouristInfoCards />
+      <Bookings />
+      <Restaurant />
+      <Footer contactDetails={contactDetails} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Restaurant.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Order from "./Order";
+
+const Restaurant = () => {
+  return (
+    <div className="list-group text-center">
+      <h3>Restaurant Orders</h3>
+      <ul className="list">
+        <Order orderType="Pizza" />
+        <Order orderType="Salads" />
+        <Order orderType="Chocolate cake" />
+      </ul>
+    </div>
+  );
+};
+
+export default Restaurant;
